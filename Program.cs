cee5342ae8c6cde180a57ff7fbb7de55fc888436
Isaac Pires <isaac.aragao.p@gmail.com>
2022-04-27 20:44:28 -0300

    recovery code

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            string[] file = File.ReadAllLines("C:\\Dev\\Faculdade\\Universe2D\\inicalTrabalho.uni");
+            string[] file = File.ReadAllLines("C:\\Users\\isaac\\Documents\\Faculdade\\AED2\\Universe2D\\inicalTrabalho.uni");
 
             string[] systemData = file[0].Split(";");
 
